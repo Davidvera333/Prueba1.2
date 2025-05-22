e301a50ed0c32d09c5d761c11e7fb899b30cca33
David Vera <davidveraruiz333@gmail.com>
2025-05-22 11:04:16 +0200

    incluyendo contenido al index.php

--- proyectos/index.php
+++ proyectos/index.php
@@ -1,1 +1,29 @@
- 
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Prueba d cosas</title>
+     <style>
+         #enviar {
+             background-color: red;
+             color: white;
+         }
+     </style>
+ </head>
+
+ <body>
+     <h1>Titulo provisional para verquetalva esto </h1>
+     <form action="" method="post">
+         <legend>Formulario-prueba</legend>
+         <label for=""></label>
+         <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+         <input type="text" name="apellido" id="apellido" placeholder="Apellido">
+         <input type="button" value="enviar" id="enviar">
+
+
+     </form>
+ </body>
+
+ </html>
